b199a800f6e288e8c3c34d8a82a316823c45cb93
Aluno.Etec <aula@etec.com>
2026-03-20 20:09:47 -0300

    tentativa de arrumar o cors, api funcionando

--- Program.cs
+++ Program.cs
@@ -1,28 +1,24 @@
 using Contest;
 using rotas;
-using MySql.Data.MySqlClient;
-
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
 
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(policy =>
-    {
-        policy
-            .WithOrigins("http://127.0.0.1:5500")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
+    options.AddPolicy("Frontend",
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
 });
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-app.UseCors();
-
+app.UseCors("Frontend");
 
 app.MapControllers();
 
@@ -31,6 +27,3 @@ app.rotasContest();
 app.rotas();
 
 app.Run();
-// hoje aprendi que posso criar classes com atributos e metodos e chamala no meu programa, ou melhor, na minha API
-//Criando noção em padrâo MVC
-//aprendi que posso colocar minha api em uma classe na m
